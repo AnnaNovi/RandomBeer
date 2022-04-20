1791a7719218af436490403eb01e7952030a1e1b
Anna <annprogram-gh@mail.ru>
2022-04-20 23:07:36 +0300

    finished with Profile component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div>Cleared project</div>
+  <ProfileBlock />
 </template>
 
 <script>
+import ProfileBlock from './components/ProfileBlock.vue';
 
 export default {
-  name: 'App'
-}
+  name: 'App',
+  components: {
+    ProfileBlock
+  }
+};
 </script>
 
 <style lang="scss">
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 #app {
   font-family: 'Times New Roman', Times, serif;
+  background: rgba(#d3d3d3, 0.5);
+  padding: 30px 80px;
+  height: 100vh;
 }
 </style>
---
+++ src/components/ProfileBlockMoreInfo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="profileBlockMoreInfo profileBlockChild">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileBlockMoreInfo',
+}
+</script>
+
+<style>
+
+</style>
